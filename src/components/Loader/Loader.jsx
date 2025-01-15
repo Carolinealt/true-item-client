18836115c6d311d49d2c5c4d7efc43abfba5b11e
Caroline Kudrytska <e.scaroline02@gmail.com>
2025-01-15 11:53:22 +0200

    loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,8 +1,24 @@
 import css from './Loader.module.css'
+import CircleLoader from "react-spinners/CircleLoader";
 
 const Loader = ({ children }) => {
+    const override = {
+        display: "block",
+        margin: "0 auto",
+        borderColor: "red",
+    };
+
     return (
-        <div className={css.loaderContainer}>{children}</div>
+        <div className={css.loaderContainer}>
+            <CircleLoader
+                color="#6ed1bc"
+                cssOverride={override}
+                size={100}
+                aria-label="Loading Spinner"
+                data-testid="loader"
+            />
+
+            {children}</div>
     )
 }
 
